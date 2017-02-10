982df4559aeefa5b32445f6a175837309255852e
Hansastro <jean-phi@laposte.net>
2017-02-10 21:30:04 +0100

    Add comments in the serial commands

--- Arduino/Focuser/Focuser.ino
+++ Arduino/Focuser/Focuser.ino
@@ -12,35 +12,41 @@ void processCommand()
   switch (SerialProtocol.getCommand().commandID)
   {
     case ML_C:
+      // Initiate temperature convertion
       // Not implemented
       break;
     case ML_FG:
+      // Goto target position
       Motor.goToTargetPosition();
       break;
     case ML_FQ:
+      // Motor stop movement
       Motor.stopMovement();
       break;
     case ML_GB:
+        // Dump value necessary to run the official moonlite software
         SerialProtocol.setAnswer(2,0x00);
     case ML_GC:
+      // Return the temperature coefficient
       // Not implemented
       break;
     case ML_GD:
+      // Return the current motor speed
       switch(Motor.getSpeed())
       {
-        case 16:
+        case 160:
           SerialProtocol.setAnswer(2,(long)20);
           break;
-        case 32:
+        case 320:
           SerialProtocol.setAnswer(2,(long)10);
           break;
-        case 63:
+        case 630:
           SerialProtocol.setAnswer(2,(long)8);
           break;
-        case 125:
+        case 1250:
           SerialProtocol.setAnswer(2,(long)4);
           break;
-        case 250:
+        case 2500:
           SerialProtocol.setAnswer(2,(long)2);
           break;
         default:
@@ -48,67 +54,82 @@ void processCommand()
       }
       break;
     case ML_GH:
+      // Return the current stepping mode (half or full step)
       SerialProtocol.setAnswer(2, (long)(Motor.getStepMode() == SC_HALF_STEP?0xFF:0x00));
       break;
     case ML_GI:
+      // get if the motor is moving or not
       SerialProtocol.setAnswer(2, (long)(Motor.isInMove()?0x01:0x00));
       break;
     case ML_GN:
+      // Get the target position
       SerialProtocol.setAnswer(4, (long)(Motor.getTargetPosition()));
       break;
     case ML_GP:
+      // Return the current position
       SerialProtocol.setAnswer(4, (long)(Motor.getCurrentPosition()));
       break;
     case ML_GT:
+      // Return the temperature
       SerialProtocol.setAnswer(4, (long)((TemperatureSensor.getTemperature()*2)));
       break;
     case ML_GV:
+      // Get the version of the firmware
       SerialProtocol.setAnswer(2, (long)(0x01));
       break;
     case ML_SC:
+      // Set the temperature coefficient
       // Not implemented
       break;
     case ML_SD:
+      // Set the motor speed
       switch(SerialProtocol.getCommand().parameter)
       {
         case 0x02:
-          Motor.setSpeed(250);
+          Motor.setSpeed(2500);
           break;
         case 0x04:
-          Motor.setSpeed(125);
+          Motor.setSpeed(1250);
           break;
         case 0x08:
-          Motor.setSpeed(63);
+          Motor.setSpeed(630);
           break;
         case 0x10:
-          Motor.setSpeed(32);
+          Motor.setSpeed(320);
           break;
         case 0x20:
-          Motor.setSpeed(16);
+          Motor.setSpeed(160);
           break;
         default:
           break;
       }
       break;
     case ML_SF:
+      // Set the stepping mode to full step
       Motor.setStepMode(SC_FULL_STEP);
       break;
     case ML_SH:
+      // Set the stepping mode to half step
       Motor.setStepMode(SC_HALF_STEP);
       break;
     case ML_SN:
+      // Set the target position
       Motor.setTargetPosition(SerialProtocol.getCommand().parameter);
       break;
     case ML_SP:
+      // Set the current motor position
       Motor.setCurrentPosition(SerialProtocol.getCommand().parameter);
       break;
     case ML_PLUS:
+      // Activate temperature compensation focusing
       // Not implemented
       break;
     case ML_MINUS:
+      // Disable temperature compensation focusing
       // Not implemented
       break;
-    case ML_PO:    
+    case ML_PO:
+      // Temperature calibration    
       // Not implemented
       break;
     default:
